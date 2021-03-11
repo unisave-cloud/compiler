0ba6568762c620a0a96421d6ba91c4a4256df54f
Jirka-Mayer <jirkamayer97@gmail.com>
2021-03-11 18:21:10 +0100

    v0.1.0

--- UnisaveCompiler/Properties/AssemblyInfo.cs
+++ UnisaveCompiler/Properties/AssemblyInfo.cs
@@ -11,4 +11,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("CompilerTests")]
 
 [assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0-dev")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
